609b8629e0f4ccadcd6ce05ad36b01c9fd12b759
yasintorun <yasintorun4680@gmail.com>
2022-02-18 10:20:59 +0300

    Added and used React Router

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { Outlet, Route, Router, Routes } from "react-router"
 import Navbar from "../components/navbar/Navbar"
 import ProblemDetailPage from "../pages/ProblemDetailPage"
 import ProblemEditor from "../pages/ProblemEditor"
@@ -9,6 +10,7 @@ const Layout = () => {
     return (
         <>
             <Navbar />
+            <Outlet />
             {/* <TrackListPage /> */}
             {/* <ProblemListPage /> */}
             {/* <ProblemDetailPage /> */}
