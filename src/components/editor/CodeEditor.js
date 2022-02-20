20d4f650fb60d8beb7e36f1206af00a2551a74c9
yasintorun <yasintorun4680@gmail.com>
2022-02-20 20:53:12 +0300

    Edited Proble editor page

--- src/components/editor/CodeEditor.js
+++ src/components/editor/CodeEditor.js
@@ -2,9 +2,10 @@ import MonacoEditor from "@monaco-editor/react";
 import { editor } from "monaco-editor";
 import React, { useCallback, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { DropdownItem, DropdownMenu, DropdownToggle, UncontrolledDropdown } from "reactstrap";
 import { ChangeLanguage } from "../../store/actions/editorActions";
 
-const CodeEditor = ({editorRef}) => {
+const CodeEditor = ({ editorRef }) => {
     const editor = useSelector(state => state.editor)
     const dispatch = useDispatch()
     const [language, setLanguage] = useState(editor.language ?? "javascript") //PS: state den gelecek.
@@ -12,7 +13,7 @@ const CodeEditor = ({editorRef}) => {
 
     const Editor = React.useMemo(() => (
         <MonacoEditor
-            height="90vh"
+            height={700}
             language={language}
             defaultValue="//test"
             onMount={(editor, monaco) => {
@@ -27,15 +28,15 @@ const CodeEditor = ({editorRef}) => {
 
     //PS: Verileri API'den çek.
     const languages = [
-        {value: "c", text: "C"},
-        {value: "cpp", text: "C++"},
-        {value: "csharp", text: "C#"},
-        {value: "java", text: "Java"},
-        {value: "python", text: "Python"},
-        {value: "javascript", text: "javascript"},
-        {value: "typescript", text: "Typescript"},
-        {value: "kotlin", text: "Kotlin"},
-        {value: "swift", text: "Swift"},
+        { value: "c", text: "C" },
+        { value: "cpp", text: "C++" },
+        { value: "csharp", text: "C#" },
+        { value: "java", text: "Java" },
+        { value: "python", text: "Python" },
+        { value: "javascript", text: "javascript" },
+        { value: "typescript", text: "Typescript" },
+        { value: "kotlin", text: "Kotlin" },
+        { value: "swift", text: "Swift" },
     ]
 
     const handleChangeLanguage = useCallback((e) => {
@@ -43,21 +44,23 @@ const CodeEditor = ({editorRef}) => {
         console.log(editorRef.current)
         dispatch(ChangeLanguage(e.target.value))
     }, [])
-    
+
+
     const SelectLanguage = React.useMemo(() => (
-        <select className="px-md-3 mb-2" onChange={handleChangeLanguage}>
-            {languages.map((lang, index) => (
-                <option selected={editor.language === lang.value} key={index} value={lang.value}>{lang.text}</option>
-            ))}
-        </select>
+        <select select className="px-md-3 mb-2" onChange={handleChangeLanguage} >
+            {
+                languages.map((lang, index) => (
+                    <option selected={editor.language === lang.value} key={index} value={lang.value}>{lang.text}</option>
+                ))
+            }
+        </select >
     ), [])
-    
+
     return (
         <div>
             <div>
                 {SelectLanguage}
             </div>
-            <hr className="mx-4"/>
             <div>
                 {Editor}
             </div>
