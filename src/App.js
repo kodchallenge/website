19e39020f21d4e9fe6385ba3aee95bd72ee8fcc2
yasintorun <yasintorun4680@gmail.com>
2022-02-20 16:15:25 +0300

    Add Route path for Problem Editor Page

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import MainPage from './pages/main/MainPage';
 import TrackListPage from './pages/TrackListPage';
 import ProblemListPage from './pages/ProblemListPage';
 import ProblemDetailPage from './pages/ProblemDetailPage';
+import ProblemEditor from './pages/ProblemEditor';
 function App() {
 
   return (
     <div className="App">
         <Routes>
+          <Route path='editor' element={<ProblemEditor />}/>
           <Route path='/' element={<Layout />}>
             <Route path='' element={<MainPage />} />
             <Route path='tracks' element={<TrackListPage />}/>
