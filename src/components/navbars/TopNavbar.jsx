9ae92421212ec5d4f82bd98e162d82e95984eec2
yasintorun <yasintorun4680@gmail.com>
2022-03-04 14:44:07 +0300

    Update TopNavbar.jsx

--- src/components/navbars/TopNavbar.jsx
+++ src/components/navbars/TopNavbar.jsx
@@ -85,7 +85,7 @@ const TopNavbar = () => {
                             <Nav className="navbar-nav-hover align-items-lg-center" navbar>
                                 <NavItem>
                                     <Link to="tracks" className='nav-link'>
-                                        Kategoriler
+                                        Problemler
                                     </Link>
                                 </NavItem>
                                 <NavItem>
@@ -96,6 +96,11 @@ const TopNavbar = () => {
                                 <NavItem>
                                     <Link to="#" className='nav-link'>
                                         Etkinlikler
+                                    </Link>
+                                </NavItem>
+                                <NavItem>
+                                    <Link to="#" className='nav-link'>
+                                        Projeler
                                     </Link>
                                 </NavItem>
                             </Nav>
@@ -155,32 +160,6 @@ const TopNavbar = () => {
                 </Container>
             </Navbar>
         </header>
-        // <nav id='top-navbar' className='top-navbar'>
-        //     <div className='p-2'>
-        //         <div className='container'>
-        //             <div className='row align-items-center'>
-        //                 <div className='col-md-3'>
-        //                     <h1><span className='text-primary'>Kod</span>Challenge</h1>
-        //                 </div>
-        //                 <div className='col-md-6'>
-        //                     <ul className='navbar align-items-center justify-content-start'>
-        //                         <li>
-        //                             <a href='#'>Anasayfa</a>
-        //                         </li>
-        //                         <li>
-        //                             <a href='#'>Problemler</a>
-        //                         </li>
-        //                     </ul>
-        //                 </div>
-        //                 <div className='col-md-3 text-end'>
-        //                     <ul>
-        //                         <li className='p-0'>Test</li>
-        //                     </ul>
-        //                 </div>
-        //             </div>
-        //         </div>
-        //     </div>
-        // </nav>
     )
 }
 
