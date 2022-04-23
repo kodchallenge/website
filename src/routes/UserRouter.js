360a01c8202802e2b1905c47ffa078d0aaaa707f
yasintorun <yasintorun4680@gmail.com>
2022-04-23 13:23:12 +0300

    Added changePassword route

--- src/routes/UserRouter.js
+++ src/routes/UserRouter.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Outlet, Route, Routes } from "react-router";
 import UserProfilePage from "../pages/user/UserProfilePage";
+import UserChangePassword from "../pages/user/UserChangePassword";
 
 const UserRouter = () => {
     return (
         <Routes>
             <Route path="/" element={<Outlet />}>
                 <Route path='profile' element={<UserProfilePage />} />
+                <Route path='change-password' element={<UserChangePassword />} />
             </Route>
         </Routes>
     )
