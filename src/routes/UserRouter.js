7b7d2ab2ab1b1c7822ba7b083191f5df6744d0da
yasintorun <yasintorun4680@gmail.com>
2022-04-23 11:04:23 +0300

    Added user profile page route

--- src/routes/UserRouter.js
+++ src/routes/UserRouter.js
@@ -1,15 +1,12 @@
 import React from "react";
 import { Outlet, Route, Routes } from "react-router";
+import UserProfilePage from "../pages/user/UserProfilePage";
 
 const UserRouter = () => {
     return (
         <Routes>
             <Route path="/" element={<Outlet />}>
-                <Route path='profile' element={<div>
-                    Kullanıcı profili.
-                    <hr />
-                    Silinecek.
-                </div>} />
+                <Route path='profile' element={<UserProfilePage />} />
             </Route>
         </Routes>
     )
